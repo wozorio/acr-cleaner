bfdd4b510dd45b2168c8e217a13c40c1a9cd83db
Wellington Ozorio <wozorio@duck.com>
2026-06-16 13:11:42 -0300

    x

--- acr_cleaner.py
+++ acr_cleaner.py
@@ -5,13 +5,12 @@
 # dependencies = [
 #     "azure-containerregistry",
 #     "azure-identity",
+#     "azure-mgmt-containerregistry",
 #     "click",
 #     "colorlog",
 #     "humanize",
 # ]
 # ///
-
-# pylint: disable=missing-module-docstring
 
 __author__ = "Wellington Ozorio <wozorio@duck.com>"
 
@@ -20,18 +19,19 @@ import datetime
 import logging
 import os
 import re
-import subprocess
+import sys
 
 import click
 import humanize
 from azure.containerregistry import ArtifactManifestOrder, ContainerRegistryClient
 from azure.identity import EnvironmentCredential
+from azure.mgmt.containerregistry import ContainerRegistryManagementClient
 from colorlog import ColoredFormatter
 
 REQUIRED_ENVIRONMENT_VARIABLES = ["AZURE_CLIENT_ID", "AZURE_CLIENT_SECRET", "AZURE_TENANT_ID", "AZURE_SUBSCRIPTION_ID"]
 
 IMAGE_ID_PATTERN = re.compile(
-    r"^(?P<registry>[a-z]+.[a-z]+.[a-z].)/(?P<repository>[a-z0-9-/]+)@(?P<digest>[sha256:]+[a-fA-F0-9]{64}$)"
+    r"^(?P<registry>[a-z0-9-]+\.[a-z]+\.[a-z]+)/(?P<repository>[a-z0-9-/]+)@(?P<digest>sha256:[a-fA-F0-9]{64})$",
 )
 
 AUDIENCE = "https://management.azure.com"
@@ -51,7 +51,7 @@ REPOS_WITH_JOB_IMAGES = [
 logger = logging.getLogger(__name__)
 
 
-@dataclasses.dataclass
+@dataclasses.dataclass(frozen=True)
 class Image:
     """Represent an image object."""
 
@@ -59,10 +59,6 @@ class Image:
     tags: list[str]
     digest: str
     age: int
-    is_dangling: bool = dataclasses.field(init=False)
-
-    def __post_init__(self) -> None:
-        self.is_dangling = self.tags is None
 
 
 def validate_image_ids(value: str) -> list[str]:
@@ -78,13 +74,16 @@ def validate_image_ids(value: str) -> list[str]:
 @click.argument("registry_resource_group")
 @click.argument("max_image_age_days", type=int)
 @click.argument("deployed_images", type=validate_image_ids)
-@click.argument("cleanup_all", type=bool)
 def main(
-    registry_name: str, registry_resource_group: str, max_image_age_days: int, deployed_images: list[str], cleanup_all: bool
+    registry_name: str,
+    registry_resource_group: str,
+    max_image_age_days: int,
+    deployed_images: list[str],
 ) -> None:
-    """
-    A script to clean up Azure container registries by deleting dangling images and images
-    which are older than a specified period of time (in days) if they are not being used.
+    """ACR cleaner script.
+
+    A script to clean up Azure container registries by deleting unused
+    images which are older than a specified period of time (in days).
     """
     setup_logging()
 
@@ -92,39 +91,40 @@ def main(
 
     registry_uri = f"https://{registry_name}.azurecr.io"
 
-    if cleanup_all:
-        logger.warning("All images except the currently deployed ones will be deleted")
-        max_image_age_days = 0
+    credential = EnvironmentCredential()
 
     logger.warning(
-        "Dangling images and unused images older than %i days will be deleted from the %s container registry",
+        "Unused images older than %i days will be deleted from the %s container registry",
         max_image_age_days,
         registry_uri,
     )
 
-    with ContainerRegistryClient(registry_uri, EnvironmentCredential(), audience=AUDIENCE) as acr_client:
+    with ContainerRegistryClient(registry_uri, credential, audience=AUDIENCE) as acr_client:
         obsolete_images = fetch_obsolete_images(acr_client, registry_uri, max_image_age_days, deployed_images)
 
         if not obsolete_images:
             logger.info("No obsolete images found for deletion")
             return
 
-        dangling_images = [image for image in obsolete_images if image.is_dangling]
-        unused_images = [image for image in obsolete_images if not image.is_dangling]
+        logger.warning("A total of %s unused images will be deleted", humanize.intcomma(len(obsolete_images)))
 
-        logger.warning("A total of %s dangling images will be deleted", humanize.intcomma(len(dangling_images)))
-        logger.warning("A total of %s unused images will be deleted", humanize.intcomma(len(unused_images)))
+        subscription_id = os.environ["AZURE_SUBSCRIPTION_ID"]
 
-        login_to_azure(
-            os.environ["AZURE_CLIENT_ID"],
-            os.environ["AZURE_CLIENT_SECRET"],
-            os.environ["AZURE_TENANT_ID"],
-            os.environ["AZURE_SUBSCRIPTION_ID"],
+        registry_usage_before_cleanup = get_registry_usage(
+            credential,
+            subscription_id,
+            registry_name,
+            registry_resource_group,
         )
 
-        registry_usage_before_cleanup = get_registry_usage(registry_name, registry_resource_group)
         delete_obsolete_images(acr_client, obsolete_images)
-        registry_usage_after_cleanup = get_registry_usage(registry_name, registry_resource_group)
+
+        registry_usage_after_cleanup = get_registry_usage(
+            credential,
+            subscription_id,
+            registry_name,
+            registry_resource_group,
+        )
 
         storage_released = registry_usage_before_cleanup - registry_usage_after_cleanup
 
@@ -153,56 +153,51 @@ def setup_logging() -> None:
 def check_env_vars(environ: dict) -> None:
     """Check whether required environment variables are set."""
     for variable in REQUIRED_ENVIRONMENT_VARIABLES:
-        if not variable in environ:
-            raise RuntimeError(f"{variable} environment variable not set")
+        if variable not in environ:
+            logger.error("%s environment variable not set", variable)
+            sys.exit(1)
 
 
 def fetch_obsolete_images(
-    acr_client: ContainerRegistryClient, registry_uri: str, max_image_age_days: int, deployed_images: list[str]
+    acr_client: ContainerRegistryClient,
+    registry_uri: str,
+    max_image_age_days: int,
+    deployed_images: list[str],
 ) -> list[Image]:
-    """Fetch a list of dangling images and unused images which are older than the `max_image_age_days` parameter."""
-    images_in_use = []
-    obsolete_images = []
+    """Fetch a list of unused images which are older than the `max_image_age_days` parameter."""
+    images_in_use: list[Image] = []
+    obsolete_images: list[Image] = []
 
     expanded_deployed_images = expand_deployed_images(acr_client, deployed_images)
+    logger.info("Fetching list of unused images older than %i days:", max_image_age_days)
 
-    logger.info("Fetching list of dangling images and unused images older than %i days:", max_image_age_days)
     for repository in acr_client.list_repository_names():
         if not is_exception_repository(repository):
             logger.info("-> Checking repository %s", repository)
-
             manifests = acr_client.list_manifest_properties(
-                repository, order_by=ArtifactManifestOrder.LAST_UPDATED_ON_DESCENDING
+                repository,
+                order_by=ArtifactManifestOrder.LAST_UPDATED_ON_DESCENDING,
             )
-
             for manifest in manifests:
                 today = datetime.datetime.now(datetime.timezone.utc)
                 image_last_update = manifest.last_updated_on
-
                 image_age_days = (today - image_last_update).days
 
-                if manifest.tags is None or (today - image_last_update) > datetime.timedelta(days=max_image_age_days):
-                    image_id = f"{registry_uri.removeprefix("https://")}/{repository}@{manifest.digest}"
+                if (today - image_last_update) > datetime.timedelta(days=max_image_age_days):
+                    image_id = f"{registry_uri.removeprefix('https://')}/{repository}@{manifest.digest}"
                     validate_image_id(image_id)
 
+                    image = Image(
+                        repository=repository,
+                        tags=manifest.tags,
+                        digest=manifest.digest,
+                        age=image_age_days,
+                    )
                     if image_id in expanded_deployed_images:
-                        images_in_use.append(
-                            Image(
-                                repository=repository,
-                                tags=manifest.tags,
-                                digest=manifest.digest,
-                                age=image_age_days,
-                            )
-                        )
+                        images_in_use.append(image)
                     else:
-                        obsolete_images.append(
-                            Image(
-                                repository=repository,
-                                tags=manifest.tags,
-                                digest=manifest.digest,
-                                age=image_age_days,
-                            )
-                        )
+                        obsolete_images.append(image)
+
     if images_in_use:
         logger.info(
             "The images below are older than %i days but they are in use, therefore they will not be deleted:",
@@ -244,61 +239,20 @@ def expand_deployed_images(acr_client: ContainerRegistryClient, deployed_images:
     return expanded_deployed_images
 
 
-def login_to_azure(client_id: str, client_secret: str, tenant_id: str, subscription_id: str) -> None:
-    """Login to Azure for subsequent azure-cli commands."""
-    logger.info("Logging in to Azure")
-    run_os_command(
-        [
-            "az",
-            "login",
-            "--service-principal",
-            "-u",
-            client_id,
-            "-p",
-            client_secret,
-            "--tenant",
-            tenant_id,
-        ]
-    )
-    select_subscription(subscription_id)
+def get_registry_usage(
+    credential: EnvironmentCredential,
+    subscription_id: str,
+    registry_name: str,
+    resource_group: str,
+) -> int:
+    """Return the storage quota usage of the container registry using the Management SDK."""
+    client = ContainerRegistryManagementClient(credential=credential, subscription_id=subscription_id)
 
+    usages = client.registries.list_usages(resource_group, registry_name)
+    quota_usage = next((item["currentValue"] for item in usages["value"] if item["name"] == "Size"), None)
 
-def select_subscription(subscription_id: str) -> None:
-    """Set an Azure subscription as active."""
-    logger.info("Setting Azure subscription %s as active", subscription_id)
-    run_os_command(["az", "account", "set", "--subscription", subscription_id])
-
-
-def get_registry_usage(registry_name: str, resource_group: str) -> int:
-    """Return the quota usage of the container registry."""
-    quota_usage = run_os_command(
-        [
-            "az",
-            "acr",
-            "show-usage",
-            "--name",
-            registry_name,
-            "--resource-group",
-            resource_group,
-            "--output",
-            "json",
-            "--query",
-            "value[0].currentValue",
-        ],
-    )
     logger.info("The current quota usage on %s is %s", registry_name, humanize.naturalsize(quota_usage))
-
     return int(quota_usage)
-
-
-def run_os_command(cmd: list[str]) -> str:
-    """Wrapper to run OS commands."""
-    try:
-        output = subprocess.run(cmd, capture_output=True, check=True).stdout.decode()
-    except subprocess.CalledProcessError as error:
-        logger.exception(error)
-
-    return output.strip()
 
 
 def delete_obsolete_images(acr_client: ContainerRegistryClient, obsolete_images: list[Image]) -> None:
@@ -324,9 +278,9 @@ def validate_image_id(image_id: str) -> None:
     match = IMAGE_ID_PATTERN.match(image_id)
 
     if not match:
-        raise ValueError(f"Image URI format {image_id} is not valid")
+        logger.error("Image URI format %s is not valid", image_id)
+        sys.exit(1)
 
 
 if __name__ == "__main__":
-    # pylint: disable=no-value-for-parameter
     main()
